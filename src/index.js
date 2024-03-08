112b19946feefd1ad36a755a0e9c338d3c773e7a
Siraj34 <156498531+Siraj34@users.noreply.github.com>
2024-03-08 19:59:51 +0000

    ALL PRODUCT

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import { store } from './Store/store'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PayPalScriptProvider deferLoading={true}>
+        <App />
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 )
